001060232e481af88e705ed12e79da5385a0485d
mtar347 <105006877+mtar347@users.noreply.github.com>
2026-07-08 13:53:29 -0400

    add class12

---
+++ class-demos/class12/script.js
@@ -0,0 +1,15 @@
+// What is the first line of code we write in a .js file?
+// Write it below this line.
+window.addEventListener("load", ()=>{
+
+// Inside of the function, retrieve the id of the div you created in index.html and assign it to a variable.
+let con = document.getElementById('container')
+// Q: What is another way to write the first line of code? 
+// A:// window.onload = () =>{}
+
+// Q: What does the document keyword represent?
+// A: html document or DOM
+
+// Q: What is the property we access to change the style of a selected element in js? How would we change the background color of the div we selected?
+// A:document.querySelector().style.backgroundColor = '# '
+}
